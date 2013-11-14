1f0e66abfbe73a46dd220954d6c409da50faf44a
James <jamesdjuhartono@gmail.com>
2013-11-14 21:15:43 +0800

    Refactored authentication method. Specify auth file in absolute path (useful for task scheduling).

--- src/auth.py
+++ src/auth.py
@@ -1,18 +1,38 @@
 import config
+import json
 import requests
-import os
+import getpass
 
 
-def validate(token):
+def login():
+    payload = {}
+    payload['__VIEWSTATE'] = config.viewstate #Should be removed once browser based auth is in place
+    payload['userid'] = raw_input('User ID: ')
+    payload['password'] = getpass.getpass()
+
+    url = config.ivlelogin + config.APIKey
+    response = requests.post(url, data=payload, allow_redirects=True)
+
+    if len(response.text) != 416: #This too hackish, will try to do proper browser based authentication
+        print "Wrong username or password, please try again!"
+        return "NULL"
+    else:
+        with open(config.authfile, 'w') as fi:
+            fi.write('{"Token": "' + response.text + '"}') #rewrite the token with new value
+        print "Login success"
+        return response.text
+
+def isValid(token):
     url = config.hosturl + "Validate"
     payload = {'APIKey': config.APIKey, 'Token': token}
     response = requests.get(url, params=payload)
+    return response.json()['Success']
 
-    data = response.json()
-    if data['Success'] is False:
-        os.remove('auth.json')
-        return False
-    else:
-        f = open('auth.json', 'w')
-        f.write(response.text)
-        return True
+def authenticate():
+    with open(config.authfile) as fi:
+        token = json.load(fi)['Token']
+        
+    while not isValid(token):
+        token = login()
+
+    return token
--- src/downloader.py
+++ src/downloader.py
@@ -1,48 +1,21 @@
-import requests
-import getpass
+import os
 import config
 import auth
-import json
 import modules
 import workbin
-import os
 
 
-def login():
-    payload = {}
-    payload['__VIEWSTATE'] = config.viewstate
+def setup():
+    if not os.path.exists(config.credentialdir):
+        os.makedirs(config.credentialdir)
 
-    while True:
-        userid = raw_input('User ID: ')
-        payload['userid'] = userid
-        password = getpass.getpass()
-        payload['password'] = password
+    if not os.path.exists(config.authfile):
+        with open(config.authfile, 'w') as fi:
+            fi.write('{"Token": "NULL"}') #init of empty token
 
-        url = config.formhost + config.APIKey
-        response = requests.post(url, data=payload, allow_redirects=True)
-
-        if len(response.text) != 416:
-            print "Wrong username or password, please try again!"
-        else:
-            print "Login success"
-            auth.validate(response.text)
-            break
-
-currdir = os.getcwd()
-authdir = os.path.join(currdir, "auth.json")
-
-while True:
-    if os.path.exists(authdir):
-        with open(authdir) as fi:
-            data = json.load(fi)
-            token = data['Token']
-            if auth.validate(token) is False:
-                login()
-
-        modules = modules.getModules(token)
-        workbin.getFiles(modules, token)
-        break
-    else:
-        login()
-
-end = raw_input("Download complete. Press any key to exit.")
+if __name__ == '__main__':
+    
+    setup()
+    token = auth.authenticate()
+    modules = modules.getModules(token)
+    workbin.getFiles(modules, token)
